467e902001b90e3e6271875a3da9451c80b38838
Cfakir3 <cfakir3@gatech.edu>
2020-09-22 23:56:56 -0400

    Summary with change

--- app/src/main/java/com/demo/weather/SettingsFragment.java
+++ app/src/main/java/com/demo/weather/SettingsFragment.java
@@ -8,7 +8,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     private void setPreferenceSum( Preference p , Object v){
         String value = v.toString();
         String key = p.getKey();
@@ -32,6 +32,26 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             // Only Edit text and List Pref
             String v = sharedPreferences.getString(p.getKey(), "");
             setPreferenceSum(p , v);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Preference p = findPreference(s);
+        if(p !=null){
+            setPreferenceSum(p , sharedPreferences.getString(p.getKey(), ""));
         }
     }
 }
