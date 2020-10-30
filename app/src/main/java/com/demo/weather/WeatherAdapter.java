86148647e4d3565a53a44d7d1529e2eb569d3f14
Cfakir3 <cfakir3@gatech.edu>
2020-10-30 12:53:01 -0400

    Added polish to today weather

--- app/src/main/java/com/demo/weather/WeatherAdapter.java
+++ app/src/main/java/com/demo/weather/WeatherAdapter.java
@@ -20,8 +20,11 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
    private final Context mContext;
    private Cursor mCursor;
    final WeatherAdapterOnClickHandler mClickHandler;
+   private final int VIEW_TYPE_TODAY = 5;
+   private final int VIEW_TYPE_FUTURE = 2;
+   private final boolean mUseTodayLayout;
 
-   public interface WeatherAdapterOnClickHandler{
+    public interface WeatherAdapterOnClickHandler{
        void onClick(long weatherLine);
     }
 
@@ -30,6 +33,7 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
         //later
         mContext = c;
         mClickHandler = clickHandler;
+        mUseTodayLayout=mContext.getResources().getBoolean(R.bool.today_layout);
     }
 
     @NonNull
@@ -37,10 +41,27 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
     public WeatherAdapterViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
         // Things to happen on ViewHolder is created
         // Executed when RecyclerView is laid out to full screen
+        int viewTypeId;
         Context c = viewGroup.getContext();
-        int layout = R.layout.weather_list_item;
+
+
+        switch(i){
+            case VIEW_TYPE_TODAY:{
+                viewTypeId=R.layout.list_item_weather_today;
+                break;}
+
+            case VIEW_TYPE_FUTURE:{
+                viewTypeId=R.layout.weather_list_item;
+                break;}
+            default:
+                throw new IllegalArgumentException("Invalid viewtype , val :" + i);
+        }
+
+
+        //int layout = R.layout.weather_list_item;
         LayoutInflater inflate = LayoutInflater.from(c);
-        View v = inflate.inflate(layout,viewGroup, false);
+        View v = inflate.inflate(viewTypeId,viewGroup, false);
+        v.setFocusable(true);
         return new WeatherAdapterViewHolder(v);
     }
 
@@ -51,6 +72,8 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
         //String weatherDataLine = mWeatherData.get(i);
         //weatherAdapterViewHolder.mWeatherTextView.setText(weatherDataLine);
         mCursor.moveToPosition(i);
+        int weatherImageId;
+        int viewType = getItemViewType(i);
 
         long dateInMillis = mCursor.getLong(MainActivity.INDEX_WEATHER_DATE);
         /* Get human readable string using our utility method */
@@ -64,7 +87,7 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
         double lowInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMP);
         double humudity =  mCursor.getDouble(MainActivity.INDEX_WEATHER_HUM);
 
-        int weatherImageId = FormatUtils.getSmallArtResourceIdForWeatherCondition(weatherId);
+        //int weatherImageId = FormatUtils.getSmallArtResourceIdForWeatherCondition(weatherId);
 
         String humidString = FormatUtils.formatHumidity(mContext, humudity);
         String highString = FormatUtils.formatTemp(mContext, highInCelsius);
@@ -72,15 +95,41 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
         //String highAndLowTemperature = FormatUtils.formatHighLows(mContext, highInCelsius, lowInCelsius);
 
         //String weatherSummary = dateString + " - " + highAndLowTemperature+" "+FormatUtils.formatHumidity(mContext, humudity);
-        weatherAdapterViewHolder.iconView.setImageResource(weatherImageId);
+        //weatherAdapterViewHolder.iconView.setImageResource(weatherImageId);
         weatherAdapterViewHolder.dateView.setText(dateString);
         weatherAdapterViewHolder.humidity.setText(humidString);
         weatherAdapterViewHolder.highTempView.setText(highString);
         weatherAdapterViewHolder.lowTempView.setText(lowString);
 
+        switch (viewType){
+            case VIEW_TYPE_TODAY:{
+                weatherImageId=FormatUtils.getLargeArtResourceIdForWeatherCondition(weatherId);
+                break;}
+            case VIEW_TYPE_FUTURE:{
+                weatherImageId=FormatUtils.getSmallArtResourceIdForWeatherCondition(weatherId);
+                break;}
+            default:
+                throw new IllegalArgumentException("Invalid viewtype , val :" + i);
 
+
+        }
+        weatherAdapterViewHolder.iconView.setImageResource(weatherImageId);
     }
     // Gets number of items to be used by RecylerView
+
+
+    @Override
+    public int getItemViewType(int position) {
+        //return super.getItemViewType(position);
+        if(mUseTodayLayout && position==0){
+            return VIEW_TYPE_TODAY;
+        }
+        else{
+            return  VIEW_TYPE_FUTURE;
+        }
+
+    }
+
     @Override
     public int getItemCount() {
         if(mCursor==null){
@@ -108,6 +157,8 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherA
             dateView = (TextView) itemView.findViewById(R.id.date);
 
             itemView.setOnClickListener(this);
+
+
         }
 
         @Override
